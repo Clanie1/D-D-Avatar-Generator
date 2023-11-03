ae6f7bfa10b8ef455f53a28a7cd5f7337a2617a3
Clanie1 <danybarociov@gmail.com>
2023-11-02 20:39:09 -0700

    init

---
+++ src/test/java/com/example/dungeons_dragons/DungeonsDragonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dungeons_dragons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsDragonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
